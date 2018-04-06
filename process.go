b4677e6003f686f433cfcb31718dff36e12f4c18
deBreaker <wang.yannan@outlook.com>
2018-04-06 17:28:47 +0800

    refine interface

--- process.go
+++ process.go
@@ -19,6 +19,9 @@ type Process interface {
 	// Executable name running this process. This is not a path to the
 	// executable.
 	Executable() string
+
+	// The command to launch the current process.
+	Cmdline() string
 }
 
 // Processes returns all processes.
